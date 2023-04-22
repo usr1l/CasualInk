154c4657dc6ea68ed113f839c5e188e0f191a078
TKNY <zhtony0398@gmail.com>
2023-04-22 01:04:56 -0400

    feature banner bug fix

--- react-app/src/components/LoginFormModal/index.js
+++ react-app/src/components/LoginFormModal/index.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { login } from "../../store/session";
 import { useDispatch } from "react-redux";
 import { useModal } from "../../context/Modal";
+import InputDiv from "../InputDiv";
 import "./LoginForm.css";
 
 function LoginFormModal() {
@@ -22,35 +23,44 @@ function LoginFormModal() {
   };
 
   return (
-    <>
-      <h1>Log In</h1>
-      <form onSubmit={handleSubmit}>
-        <ul>
+    <div id='log-in'>
+      <h1 id="log-in__title">Welcome Back!</h1>
+      <h2 id='log-in__h2'>We're so excited to see you again!</h2>
+      <form id='log-in__form' onSubmit={handleSubmit}>
+        <ul id='log-in__error-list'>
           {errors.map((error, idx) => (
             <li key={idx}>{error}</li>
           ))}
         </ul>
-        <label>
-          Email
-          <input
-            type="text"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
+        <label id='log-in__label'>
+          EMAIL
         </label>
-        <label>
-          Password
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
+        <input
+          className='log-in__input'
+          type="text"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <label id='log-in__label'>
+          PASSWORD
         </label>
-        <button type="submit">Log In</button>
+        <input
+          className='log-in__input'
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <InputDiv>
+
+        </InputDiv>
+        <InputDiv>
+
+        </InputDiv>
       </form>
-    </>
+      <button type="submit">Log In</button>
+    </div>
   );
 }
 
