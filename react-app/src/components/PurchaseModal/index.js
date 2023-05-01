386fb729e1a2ea258cb59f286e1a9855524d73ed
TKNY <zhtony0398@gmail.com>
2023-05-01 04:06:36 -0400

    remove normalize

--- react-app/src/components/PurchaseModal/index.js
+++ react-app/src/components/PurchaseModal/index.js
@@ -1,11 +1,18 @@
 import React, { useState } from 'react';
 import "./PurchaseModal.css";
 import InputDiv from '../InputDiv';
+import Button from '../Button';
 
 const PurchaseModal = () => {
   const [ cardNumber, setCardNumber ] = useState("");
   const [ expiryDate, setExpiryDate ] = useState("");
   const [ csv, setcsv ] = useState("");
+
+  const [ validationErrors, setValidationErrors ] = useState({});
+
+  // const handleCheckout = () => {
+  //   if
+  // };
 
   return (
     <div className='purchase-modal'>
@@ -18,13 +25,13 @@ const PurchaseModal = () => {
         >
           <input
             className='__input'
-            type="number"
+            type="text"
             value={cardNumber}
             onChange={(e) => setCardNumber(e.target.value)}
             required
           />
         </InputDiv>
-        <InputDiv label="Expiry Date: "
+        <InputDiv label="Expiry Date: *"
           divStyle={'input--wide'}
           labelStyle={'__label'}
         // error={validationErrors.auctionDeadline}
@@ -37,20 +44,27 @@ const PurchaseModal = () => {
           // disabled={inputDisableBool}
           />
         </InputDiv>
-        <InputDiv label="CSV: "
+        <InputDiv label="CSV: *"
           divStyle={'input--wide'}
           labelStyle={'__label'}
         // error={validationErrors.auctionDeadline}
         >
           <input
             className='__input'
-            type='number'
+            type='text'
             value={csv}
             onChange={(e) => setcsv(e.target.value)}
           // disabled={inputDisableBool}
           />
         </InputDiv>
       </div>
+      <Button
+        buttonSize={"btn--splash"}
+        buttonStyle={"btn--demo"}
+      // onClick={handleCheckout}
+      >
+        Buy Now
+      </Button>
     </div>
   );
 };
