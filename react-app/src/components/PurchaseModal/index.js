077a9d66f775bc6f65bd6cbb14aab4c5d6add735
TKNY <zhtony0398@gmail.com>
2023-05-01 01:19:24 -0400

    latest

---
+++ react-app/src/components/PurchaseModal/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./PurchaseModal.css";
+
+const PurchaseModal = () => {
+  return (
+    <div className='purchase-modal'>
+      <div className='form-container'>
+        <h1>Enter Payment Information</h1>
+
+      </div>
+    </div>
+  );
+};
+
+export default PurchaseModal;
