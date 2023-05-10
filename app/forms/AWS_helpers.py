d94a203d8b1f4d5469b52ce132b2574744e87741
TKNY <zhtony0398@gmail.com>
2023-05-10 18:04:44 -0400

    BUCKET_NAME

--- app/forms/AWS_helpers.py
+++ app/forms/AWS_helpers.py
@@ -4,6 +4,7 @@ import os
 import uuid
 
 BUCKET_NAME = os.environ.get("S3_BUCKET")
+print("BUCKKEEETTTTTNAMEEEEEEEEEEEEEEEEEE", BUCKET_NAME)
 S3_LOCATION = f"https://{BUCKET_NAME}.s3.amazonaws.com/"
 ALLOWED_EXTENSIONS = {"pdf", "png", "jpg", "jpeg", "gif"}
 
